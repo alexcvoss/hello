3c6641deee3b034534fdfc28ae2f442954ddafd4
Alexander Voss <avoss14@huskers.unl.edu>
2024-08-29 12:47:39 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Alexander Voss
+ *
+ * Date: 2024/08/29
+ *
+ * a hello world program that prints my name
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Alexander Voss\n");
+
+    return 0;
+}
