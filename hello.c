d7a0174966b07f000ef6732b8e7667ae40472b97
Drew Moser <dmoser4@huskers.unl.edu>
2024-08-30 09:49:12 -0500

    added my major to the output

--- hello.c
+++ hello.c
@@ -14,6 +14,7 @@ int main(int argc, char **argv) {
 
     printf("Alexander Voss!!!!!!!\n");
     printf("Computer Engineering\n");
-
+    printf("Hello my name is Drew Moser!\n");
+    printf("I am also majoring in Computer Engineering\n");
     return 0;
 }
